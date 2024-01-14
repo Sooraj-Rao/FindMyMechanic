2782decc1f65314c349c70be339ca787ef58ff05
Sooraj-Rao <143273952+Sooraj-Rao@users.noreply.github.com>
2024-01-14 12:01:26 +0530

    #FD81OMACCESV

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { UserRouter } from "./Routes/User.js";
-import { BookServiceRouter } from "./Routes/BookService.js";
+import { UserRouter } from "./src/Routes/User.js";
+import { BookServiceRouter } from "./src/Routes/BookService.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+let DB_URL = process.env.DATABASE;
 
 app.use(express.json());
 app.use(cors());
@@ -16,7 +17,6 @@ app.use("/", UserRouter);
 app.use("/BookService/", BookServiceRouter);
 
 try {
-  let DB_URL = process.env.DATABASE;
   mongoose.connect(DB_URL);
   console.log("Connected to Database");
 } catch (error) {
